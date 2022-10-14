3ea567f4bf7d79b357481652e1689e129e3b1827
Paulamt2022 <paula.martinez1.mt@usa.edu.co>
2022-10-14 12:18:05 -0500

    Add Reto3 actual si

--- src/main/java/doctor/doctor/DoctorApplication.java
+++ src/main/java/doctor/doctor/DoctorApplication.java
@@ -1,15 +1,14 @@
 package doctor.doctor;
 
-import doctor.doctor.model.Doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
+@SpringBootApplication
 public class DoctorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Doctor.class, args);
+		SpringApplication.run(DoctorApplication.class, args);
 	}
 
 }
